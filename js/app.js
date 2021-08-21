a02a6e1bfc5ed7b5d4e42a0e1bef14db9d25e32f
rdrayhan7916 <rdrayhan7916@gmail.com>
2021-08-21 19:29:42 +0600

    finished

--- js/app.js
+++ js/app.js
@@ -8,36 +8,65 @@ const storageCost = document.getElementById('storage-cost')
 const deliveryCost = document.getElementById('delivery-cost')
 const totalCost = document.getElementById('total-cost')
 const productPrice = document.getElementById('product-price')
+const kuponPrice = document.getElementById('kupon-price')
+
+
 //  ----memory-option----
 document.getElementById('8gb-memory-fee').addEventListener('click', function () {
     memoryCost.innerText = '0'
     updateTotal()
+    LastTotalUpdate()
 })
 document.getElementById('16gb-memory-fee').addEventListener('click', function () {
-    memoryCost.innerText = '20'
+    memoryCost.innerText = '180'
     updateTotal()
+    LastTotalUpdate()
 })
 
 //---- storage-option---
 document.getElementById('256gb-ssd-fee').addEventListener('click', function () {
-    storageCost.innerText = '20'
+    storageCost.innerText = '0'
     updateTotal()
+    LastTotalUpdate()
 })
 document.getElementById('512gb-ssd-fee').addEventListener('click', function () {
-    storageCost.innerText = '50'
-    updateTotal()
-})
-document.getElementById('1tb-ssd-fee').addEventListener('click', function () {
     storageCost.innerText = '100'
     updateTotal()
+    LastTotalUpdate()
+})
+document.getElementById('1tb-ssd-fee').addEventListener('click', function () {
+    storageCost.innerText = '180'
+    updateTotal()
+    LastTotalUpdate()
 })
 
 // ----delivery-option----
 document.getElementById('normal-delivery').addEventListener('click', function () {
-    deliveryCost.innerText = '1'
+    deliveryCost.innerText = '0'
     updateTotal()
+    LastTotalUpdate()
 })
 document.getElementById('emergency-delivery').addEventListener('click', function () {
     deliveryCost.innerText = '20'
     updateTotal()
+    LastTotalUpdate()
+
+})
+
+function LastTotalUpdate() {
+    kuponPrice.innerText = totalCost.innerText;
+    return kuponPrice.innerText;
+}
+
+// kupon
+
+document.getElementById('apply-btn').addEventListener('click', function () {
+    const kupon = document.getElementById('kupon')
+
+    if (kupon.value == ('stevekaku')) {
+        const discount = parseInt(totalCost.innerText) / 5;
+        kuponPrice.innerText = parseInt(totalCost.innerText) - discount;
+
+    }
+    kupon.value = ''
 })
